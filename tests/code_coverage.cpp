4292c3cd0a4e97e94b828189ae3328317dec6939
Sylvestre Ledru <sledru@mozilla.com>
2020-04-24 23:00:48 +0200

    add missing requires

--- tests/code_coverage.cpp
+++ tests/code_coverage.cpp
@@ -1,10 +1,12 @@
 // Test code coverage
-
+//
 // RUN: mkdir -p %t/test && cd %t/test
 // RUN: %clang --coverage %s -o foo
 // RUN: ./foo
 // RUN: test -f code_coverage.gcno
 // RUN: test -f code_coverage.gcda
+// REQUIRES: clang
+
 #include <stdio.h>
 int main() {
 if (1==1) {
