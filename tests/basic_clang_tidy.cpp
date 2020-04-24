1609f7a3438cff816dc1fe4d3a84565d6e230e49
Sylvestre Ledru <sylvestre@debian.org>
2020-04-24 22:05:57 +0200

    Add REQUIRES: clang-tidy

--- tests/basic_clang_tidy.cpp
+++ tests/basic_clang_tidy.cpp
@@ -1,6 +1,7 @@
 // Test check tidy check
 //
 // RUN: %clang_tidy -checks='modernize-concat-nested-namespaces'  %s -extra-arg=-std=c++17 | grep "nested namespaces can"
+// REQUIRES: clang-tidy
 
 namespace mozilla {
 namespace dom {
