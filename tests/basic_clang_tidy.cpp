ed87e5ece72a75669148fd67c64224d501d965fb
Sylvestre Ledru <sylvestre@debian.org>
2020-04-24 22:51:48 +0200

    add a comment to trigger travis

--- tests/basic_clang_tidy.cpp
+++ tests/basic_clang_tidy.cpp
@@ -1,4 +1,5 @@
 // Test check tidy check
+// Verify that a checker finds something
 //
 // RUN: %clang_tidy -checks='modernize-concat-nested-namespaces'  %s -extra-arg=-std=c++17 | grep "nested namespaces can"
 // REQUIRES: clang-tidy
