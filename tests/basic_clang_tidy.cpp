aa8f3395a9a15f91dad3b523effd26be5180227b
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2020-04-25 18:33:37 +0200

    Fix clang-tidy-feature (#11)
    Co-authored-by: serge-sans-paille <sguelton@redhat.com>

--- tests/basic_clang_tidy.cpp
+++ tests/basic_clang_tidy.cpp
@@ -1,7 +1,7 @@
 // Test check tidy check
 // Verify that a checker finds something
 //
-// RUN: %clang_tidy -checks='modernize-concat-nested-namespaces'  %s -extra-arg=-std=c++17 | grep "nested namespaces can"
+// RUN: %clang-tidy -checks='modernize-concat-nested-namespaces'  %s -extra-arg=-std=c++17 | grep "nested namespaces can"
 // REQUIRES: clang-tidy
 
 namespace mozilla {
