4fede9b6975740bb53b7c94fd49dbd380ec5da50
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2020-04-26 00:12:22 +0200

    Fix scan-build test case (#16)
    Fix requires and features and scan-view run.
    
    Co-authored-by: serge-sans-paille <sguelton@redhat.com>

--- tests/scanbuild_missing_delete.cpp
+++ tests/scanbuild_missing_delete.cpp
@@ -1,8 +1,10 @@
 // Tests scan-view runs correctly, see https://bugzilla.redhat.com/show_bug.cgi?id=1647130
-// REQUIRES: scanbuild, scanview
-// RUN: mkdir %t.out
-// RUN: %scan-build -o %t.out %clangxx -c %s -o %t
-// RUN: timeout 10 %scan-view --no-browser %t.out/*
+// REQUIRES: scan-build, scan-view
+// RUN: mkdir %t.out && cd %t.out && rm -rf *
+// RUN: %scan-build -o . %clangxx -c %s -o %t
+// It's not super satisfying to call sh like this, and not portable on windows.
+// Yet that's the only I found to cope with the lack of support for '&' in lit.
+// RUN: sh -c "%scan-view --no-browser * & WPID=$! && sleep 5 && kill $WPID"
 int main(int argc, char *argv[]) {
   while (argc--)
     new int();
