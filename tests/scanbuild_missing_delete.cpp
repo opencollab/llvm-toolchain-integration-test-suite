e823dc5fd0aa1c2f1d5cfdd7962552b4ca22425f
serge-sans-paille <sguelton@redhat.com>
2020-04-26 12:45:00 +0200

    Allow shell expression in RUN: lines

--- tests/scanbuild_missing_delete.cpp
+++ tests/scanbuild_missing_delete.cpp
@@ -1,10 +1,8 @@
 // Tests scan-view runs correctly, see https://bugzilla.redhat.com/show_bug.cgi?id=1647130
 // REQUIRES: scan-build, scan-view
-// RUN: mkdir %t.out && cd %t.out && rm -rf *
+// RUN: rm -rf %t.out && mkdir %t.out && cd %t.out && rm -rf *
 // RUN: %scan-build -o . %clangxx -c %s -o %t
-// It's not super satisfying to call sh like this, and not portable on windows.
-// Yet that's the only I found to cope with the lack of support for '&' in lit.
-// RUN: sh -c "%scan-view --no-browser * & WPID=$! && sleep 5 && kill $WPID"
+// RUN: %scan-view --no-browser * & WPID=$! && sleep 5 && kill $WPID
 int main(int argc, char *argv[]) {
   while (argc--)
     new int();
