df2e24c5d272931b2c180ac9907112f132f27deb
serge-sans-paille <sguelton@redhat.com>
2020-05-04 11:55:25 +0200

    Add missing requires for llvm_config_libs

--- tests/llvm_config_libs.cpp
+++ tests/llvm_config_libs.cpp
@@ -1,5 +1,6 @@
 // Test if llvm-config correctly finds headers and libs
 // RUN: %clangxx `%llvm-config --cxxflags --ldflags --libs` %s -o %t
+// REQUIRES: clangxx, llvm-config
 #include <llvm/IR/LLVMContext.h>
 
 int main(int argc, char** argv) {
