b4ca46f77448b5c54b1be1ace59669e44ac7220d
Sylvestre Ledru <sylvestre@debian.org>
2020-05-06 21:34:24 +0200

    add a tsan test with an heap-use-after-free error (#22)

---
+++ tests/test_asan_heap.c
@@ -0,0 +1,13 @@
+// Test asan use after free
+//
+// REQUIRES: clang, compiler-rt
+// RUN: %clang -o %t -fsanitize=address -O1 -fno-omit-frame-pointer -g %s
+// RUN: env ASAN_OPTIONS="log_path=stdout:exitcode=0"  %t 2>&1 > %t.out
+// RUN: grep -q "heap-use-after-free" %t.out
+
+#include <stdlib.h>
+int main() {
+  char *x = (char*)malloc(10 * sizeof(char*));
+  free(x);
+  return x[5];
+}
