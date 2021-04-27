787bac3b85e41b591c8abf96b27f1ebd87e13191
serge-sans-paille <sguelton@redhat.com>
2021-04-27 14:13:58 +0200

    Fix clang-format-diff test

--- tests/format_diff.c
+++ tests/format_diff.c
@@ -3,10 +3,13 @@
 // REQUIRES: clang-format-diff, clang
 // RUN: %clang -E -Wp,-P %s -DV0 > %t.0.c
 // RUN: %clang -E -Wp,-P %s -DV1 > %t.1.c
-// RUN: diff -u %t.0.c %t.1.c %clang-format-diff
+// RUN: diff -u %t.0.c %t.1.c || true > %t.patch
+// RUN: %clang-format-diff -i < %t.patch
 
 #if defined(V0)
-int main() { }
+int main() {
+}
 #elif defined(V1)
-int main() { return 0; }
+int main() { return 0;
+}
 #endif
