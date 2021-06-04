4f00ea98b6943698c36368b54797a6fdc3a6c39b
Sylvestre Ledru <sylvestre@debian.org>
2021-06-04 11:32:30 +0200

    add a test to link libc++ as static (fails for now)

---
+++ tests/libc++_link_static.cpp
@@ -0,0 +1,17 @@
+// Test libc++ static
+// https://bugs.llvm.org/show_bug.cgi?id=43604
+//
+// RUN: %clangxx -o %t %s -pie -static-libstdc++
+// RUN: %t
+// ldd %t 2>&1|grep -qv libc++.so.1
+// ldd %t 2>&1|grep -qv libc++abi.so.1
+//
+// For now, the next command fails:
+// RUN: %clangxx -o %t %s -pie -stdlib=libc++ -static-libstdc++
+// REQUIRES: clangxx, libc++
+// XFAIL: *
+
+#include <iostream>
+int main () {
+    std::cout << "Hello World!" << std::endl;
+}
