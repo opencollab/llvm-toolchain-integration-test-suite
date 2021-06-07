8ec743cd464d46b1d5054771f5c659ff14f0c08a
Sylvestre Ledru <sylvestre@debian.org>
2021-06-07 22:32:22 +0200

    rpass_inline.cpp: update of the tests for clang 13
    With clang 13, the previous tests didn't trigger anything.
    With the full test, it is now back.
    
    Probably caused by
    https://reviews.llvm.org/D94825
    or
    https://reviews.llvm.org/D94333

--- tests/rpass_inline.cpp
+++ tests/rpass_inline.cpp
@@ -7,3 +7,11 @@
 int foo(int x, int y) __attribute__((always_inline));
 int foo(int x, int y) { return x + y; }
 int bar(int j) { return foo(j, j - 2); }
+
+int sum = 0;
+
+int main(int argc, const char *argv[]) {
+  for (int i = 0; i < 30; i++)
+    bar(argc);
+  return sum;
+}
