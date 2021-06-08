21519b4140536b244f1fcc9f2acdd8da342c12fa
Sylvestre Ledru <sylvestre@debian.org>
2021-06-08 10:33:11 +0200

    rpass_inline: adjust the test for 13

--- tests/rpass_inline.cpp
+++ tests/rpass_inline.cpp
@@ -1,7 +1,7 @@
 // Test a Rpass=inline
 //
 // RUN: %clangxx -O2 -Rpass=inline %s -c &> %t.log
-// RUN: grep -q "cost=always" %t.log
+// RUN: grep -q -E "(inlined into main with|cost=always)" %t.log
 // REQUIRES: clangxx
 
 int foo(int x, int y) __attribute__((always_inline));
