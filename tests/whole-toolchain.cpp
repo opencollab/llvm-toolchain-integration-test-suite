99dd6eab39bc37f8f538c9c3f69e906125cc8c85
serge-sans-paille <sguelton@redhat.com>
2021-06-11 11:34:22 +0200

    Activate compiler-rt testing
    Current workarounds:
    - rely on libgcc_eh to provide unwinding symbols

--- tests/whole-toolchain.cpp
+++ tests/whole-toolchain.cpp
@@ -1,6 +1,9 @@
 // Tests all components of the toolchain
 // REQUIRES: clang, lld, compiler-rt, libc++
-// RUN: %clangxx -fuse-ld=lld -rtlib=compiler-rt -stdlib=libc++ %s -o %t
+//
+// NOTE: Adding -lgcc_eh here is required to provide unwinding informations. An
+// alternative would be to force usage of LLVM unwinder when building compiler-rt.
+// RUN: %clangxx -fuse-ld=lld -rtlib=compiler-rt -stdlib=libc++ -lgcc_eh %s -o %t
 // RUN: %t | grep "Hello World"
 
 #include <iostream>
