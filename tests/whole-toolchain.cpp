0aaa9e9ad235071cebcbc0b25972677e1587a78e
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-06-21 20:37:06 +0000

    Enable compiler-rt (#43)
    * Activate compiler-rt testing
    
    Current workarounds:
    - rely on libgcc_eh to provide unwinding symbols
    
    Co-authored-by: serge-sans-paille <sguelton@redhat.com>
    Co-authored-by: Sylvestre Ledru <sylvestre@debian.org>

--- tests/whole-toolchain.cpp
+++ tests/whole-toolchain.cpp
@@ -1,6 +1,9 @@
 // Tests all components of the toolchain
 // REQUIRES: clang, lld, compiler-rt, libc++
-// RUN: %clangxx -fuse-ld=lld -rtlib=compiler-rt -stdlib=libc++ %s -o %t
+//
+// NOTE: Adding -lgcc_eh here is required to provide unwinding information. An
+// alternative would be to force usage of LLVM unwinder when building compiler-rt.
+// RUN: %clangxx -fuse-ld=lld -rtlib=compiler-rt -stdlib=libc++ -lgcc_eh %s -o %t
 // RUN: %t | grep "Hello World"
 
 #include <iostream>
@@ -8,4 +11,3 @@ int main(int argc, char **argv) {
   std::cout << "Hello World" << std::endl;
   return 0;
 }
-
