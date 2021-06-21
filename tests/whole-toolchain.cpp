452966be3348425309f955623fc680dc2bef1566
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-06-21 19:39:54 +0000

    Update tests/whole-toolchain.cpp
    Co-authored-by: Sylvestre Ledru <sylvestre@debian.org>

--- tests/whole-toolchain.cpp
+++ tests/whole-toolchain.cpp
@@ -1,7 +1,7 @@
 // Tests all components of the toolchain
 // REQUIRES: clang, lld, compiler-rt, libc++
 //
-// NOTE: Adding -lgcc_eh here is required to provide unwinding informations. An
+// NOTE: Adding -lgcc_eh here is required to provide unwinding information. An
 // alternative would be to force usage of LLVM unwinder when building compiler-rt.
 // RUN: %clangxx -fuse-ld=lld -rtlib=compiler-rt -stdlib=libc++ -lgcc_eh %s -o %t
 // RUN: %t | grep "Hello World"
@@ -11,4 +11,3 @@ int main(int argc, char **argv) {
   std::cout << "Hello World" << std::endl;
   return 0;
 }
-
