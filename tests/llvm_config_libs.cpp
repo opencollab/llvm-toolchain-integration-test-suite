d8e3d7c17426778213e3c66809a1d21991505ccd
Sylvestre Ledru <sylvestre@debian.org>
2021-07-10 21:55:19 +0200

    llvm-config: Check that warnings are not exported

--- tests/llvm_config_libs.cpp
+++ tests/llvm_config_libs.cpp
@@ -1,6 +1,10 @@
 // Test if llvm-config correctly finds headers and libs
 // RUN: %clangxx `%llvm-config --cxxflags --ldflags --libs` %s -o %t
 // RUN: %clangxx `%llvm-config  --link-shared --cxxflags --ldflags --libs` %s -o %t
+//
+// Check that warnings are not exported
+// RUN: %llvm-config --cxxflags | grep -v " \-W"
+//
 // REQUIRES: clangxx, llvm-config
 #include <llvm/IR/LLVMContext.h>
 
