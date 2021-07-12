5d09c9f84ddd8a6d784c3730a60fa26756416b65
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-07-12 18:11:37 +0000

    Update llvm-ir.c

--- tests/llvm-ir.c
+++ tests/llvm-ir.c
@@ -1,9 +1,9 @@
 // Test LLVM IR
-// RUN: %clang -S -emit-llvm %s -o a.ll
-// RUN: %llc a.ll && touch a.s
+// RUN: %clang -S -emit-llvm %s -o %t.ll
+// RUN: %llc %t.ll && touch %t.s
 // RUN: %lli a.ll|grep -q "lli foo"
-// RUN: %opt -S -O3 a.ll -o opt.ll
-// RUN: %lli opt.ll|grep -q "lli foo"
+// RUN: %opt -S -O3 %t.ll -o %t.opt.ll
+// RUN: %lli %t.opt.ll | grep -q "lli foo"
 // REQUIRES: clang, llc, lli, opt
 
 #include <stdio.h>
