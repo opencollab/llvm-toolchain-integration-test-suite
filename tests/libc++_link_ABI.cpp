5f7de25f43724225cbe9989399b8a065f423deeb
Sylvestre Ledru <sylvestre@debian.org>
2021-07-31 23:02:06 +0200

    Test -l:libc++.a -l:libc++abi.a

---
+++ tests/libc++_link_ABI.cpp
@@ -0,0 +1,15 @@
+// Check that libc++ & libc++abi are built with:
+// -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON
+// -DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON
+// -DLIBCXX_STATICALLY_LINK_ABI_IN_SHARED_LIBRARY=OFF
+
+// REQUIRES: clang
+// RUN: %clang %s -o %t -stdlib=libc++ -static-libstdc++ -fuse-ld=lld -l:libc++abi.a
+// RUN: %t
+// RUN: %clang %s -stdlib=libc++ -nostdlib++ test.cpp -l:libc++.a -l:libc++abi.a -pthread -o %t
+// RUN: %t
+
+#include <iostream>
+int main() {
+  std::cout << "Hello World!" << std::endl;
+}
