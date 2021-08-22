61c08f322f25654adb705e0729db180e31b99b40
Sylvestre Ledru <sylvestre@debian.org>
2021-08-22 10:22:43 +0200

    rpass inline: Add a new output
    Also accept the new output introduced in:
    76093b17394af6f1bbe12d1fcf14ecbeb0a869df

--- tests/rpass_inline.cpp
+++ tests/rpass_inline.cpp
@@ -1,7 +1,7 @@
 // Test a Rpass=inline
 //
 // RUN: %clangxx -O2 -Rpass=inline %s -c &> tee | %t.log
-// RUN: grep -q -E "(inlined into main with|cost=always)" %t.log
+// RUN: grep -q -E "(inlined into main with|inlined into 'main' with|cost=always)" %t.log
 // REQUIRES: clangxx
 
 int foo(int x, int y) __attribute__((always_inline));
