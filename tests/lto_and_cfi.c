aa0a38a151974eda8fafa38832801db96f84eb2a
serge-sans-paille <sguelton@redhat.com>
2022-02-23 17:37:06 +0100

    Add host architecture to the available features
    So that we can XFAIL on these architectures, as done for tests/lto_and_cfi.c

--- tests/lto_and_cfi.c
+++ tests/lto_and_cfi.c
@@ -1,5 +1,6 @@
 // Test compatibility between lto and CFI, see  https://bugzilla.redhat.com/show_bug.cgi?id=1794936
 // RUN: %clang -flto -fsanitize=cfi -fvisibility=hidden %s -o %t
+// XFAIL: s390x, ppc64le
 
 int main() {
   return 0;
