ebeaa4043f5506ba6a15f9e03fa30e4895170c2a
tsai <jackalcooper@gmail.com>
2022-04-21 17:28:20 +0800

    remove files

--- tests/mlir_dialect/include/mlir/Dialect/EX/IR/EXDialect.h
+++
@@ -1,8 +0,0 @@
-#ifndef EX_EXDIALECT_H
-#define EX_EXDIALECT_H
-
-#include "mlir/IR/Dialect.h"
-
-#include "mlir/Dialect/EX/IR/EXOpsDialect.h.inc"
-
-#endif // EX_EXDIALECT_H
--- tests/mlir_dialect/lib/Dialect/EX/IR/EXOps.cpp
+++
@@ -1,6 +0,0 @@
-#include "mlir/Dialect/EX/IR/EXOps.h"
-#include "mlir/Dialect/EX/IR/EXDialect.h"
-#include "mlir/IR/OpImplementation.h"
-
-#define GET_OP_CLASSES
-#include "mlir/Dialect/EX/IR/EXOps.cpp.inc"
