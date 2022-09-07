f1f01c745452b546c991c659a44dacc1b4d2714f
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2022-09-07 20:00:37 +0000

    Update unwind.cpp

--- tests/unwind.cpp
+++ tests/unwind.cpp
@@ -1,9 +1,6 @@
 // Test libunwind - only packaged from -12
 //
 // REQUIRES: clang, libunwind, llvm-nm
-// RUN: %clangxx %s -o %t.gcc -unwindlib=libgcc
-// RUN: %llvm-nm %t.gcc | grep _Unwind_Resume
-// RUN: %t.gcc
 // RUN: %clangxx %s -o %t.llvm -unwindlib=libunwind -rtlib=compiler-rt
 // RUN: %llvm-nm %t.gcc | grep _Unwind_Resume
 // RUN: %t.llvm
