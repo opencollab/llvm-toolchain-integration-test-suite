280276c71d82765cb5b5d8fd0cf3171a1f72b91d
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2022-09-07 20:19:42 +0000

    Update unwind.cpp

--- tests/unwind.cpp
+++ tests/unwind.cpp
@@ -2,7 +2,7 @@
 //
 // REQUIRES: clang, libunwind, llvm-nm
 // RUN: %clangxx %s -o %t.llvm -unwindlib=libunwind -rtlib=compiler-rt
-// RUN: %llvm-nm %t.gcc | grep _Unwind_Resume
+// RUN: %llvm-nm %t.llvm | grep _Unwind_Resume
 // RUN: %t.llvm
 
 struct f { ~f() { }};
