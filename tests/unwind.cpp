456ad16474d9503aa55dfd41a7d3f2a2a574292b
Sylvestre Ledru <sylvestre@debian.org>
2022-09-16 15:12:51 +0200

    Test unwind features (#55)

---
+++ tests/unwind.cpp
@@ -0,0 +1,23 @@
+// Test libunwind - only packaged from -12
+//
+// REQUIRES: clang, libunwind, llvm-nm
+// NOTE: the extra -L flag is only required by Fedora
+// RUN: %clangxx %s -o %t.llvm -unwindlib=libunwind -rtlib=compiler-rt -L/usr/lib64/llvm-unwind
+// RUN: %llvm-nm %t.llvm | grep _Unwind_Resume
+// RUN: %t.llvm
+
+struct f { ~f() { }};
+int foo(int n) {
+    f  _;
+    if(n > 2)
+    throw int();
+    return 0;
+}
+int main(int argc, char** argv) {
+    try {
+      return foo(argc);
+    }
+    catch(...) {
+        return 0;
+    }
+}
