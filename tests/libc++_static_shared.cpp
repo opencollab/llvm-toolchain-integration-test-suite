486788ab58ab59cc928c0b2836eaed5a10070df3
Tulio Magno Quites Machado Filho <tuliom@redhat.com>
2023-04-27 16:40:51 -0300

    Add a test to validate libc++ static libraries in shared libraries
    Since LLVM 15.0, libc++ static libraries may not be suitable to be used
    in shared libraries because their code is position-dependent.
    In order to get a position-independent code, one has to build libc++
    with CMAKE_POSITION_INDEPENDENT_CODE=ON.

---
+++ tests/libc++_static_shared.cpp
@@ -0,0 +1,13 @@
+// Test libc++ static library linking into a shared library.
+// See https://bugzilla.redhat.com/show_bug.cgi?id=2186531
+//
+// RUN: %clangxx -c -o %t.o -fPIC %s
+// RUN: %clangxx -fuse-ld=lld -nostdlib++ -Wl,-Bstatic -lc++ -Wl,-Bdynamic -stdlib=libc++ -shared -o %t %t.o
+//
+// REQUIRES: clangxx, static-libc++
+
+#include <exception>
+
+extern void test_shared() {
+  std::get_terminate();
+}
